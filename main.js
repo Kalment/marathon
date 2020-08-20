01e39230986ac491e02dfb6c83dc06e233a7a1b1
Kalment <38328138+Kalment@users.noreply.github.com>
2020-08-20 12:35:17 +0300

    задание 1

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let a = 4; b = 5;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+function getRow(firstRow, secondRow) {
+	let countFirstRow = 0; 
+	let countSecondRow = 0;
+
+	for (let i = 0; i < firstRow.length; i++) {
+		if (firstRow.charAt(i) == 'а'){
+			countFirstRow++;
+		}
+	}
+	for (let i = 0; i < secondRow.length; i++) {
+		if (secondRow.charAt(i) == "а"){
+			countSecondRow++;
+		}
+	}
+	if (countFirstRow > countSecondRow) {
+		return firstRow;
+	} return secondRow;
+	
+}
+
+
+const firstRow = prompt("Введите первую строку");// мама мыла раму
+const secondRow = prompt("Введите вторую строку");// собака друг человека
+
+alert(getRow(firstRow, secondRow)); // мама мыла раму
