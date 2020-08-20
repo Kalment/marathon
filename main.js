ded40f4b50fe42c60de39c99b2872e07a6630220
Kalment <38328138+Kalment@users.noreply.github.com>
2020-08-20 21:48:09 +0300

    задача 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getRow(firstRow, secondRow) {
+/*function getRow(firstRow, secondRow) {
 	let countFirstRow = 0; 
 	let countSecondRow = 0;
 
@@ -22,4 +22,46 @@ function getRow(firstRow, secondRow) {
 const firstRow = prompt("Введите первую строку");// мама мыла раму
 const secondRow = prompt("Введите вторую строку");// собака друг человека
 
-alert(getRow(firstRow, secondRow)); // мама мыла раму
+alert(getRow(firstRow, secondRow)); // мама мыла раму*/
+
+
+
+
+function formattedPhone(phone) {
+	let formatPhone = '';
+	for (let i = 0; i < phone.length; i++){
+		if (i === 2) {
+			formatPhone += ' (';
+		} else if (i === 5){
+			formatPhone += ') ';
+		} else if (i === 8){
+			formatPhone += '-';
+		} else if (i === 10){
+			formatPhone += '-';
+		}
+		formatPhone += phone.charAt(i);
+	}
+	return formatPhone;
+}
+
+
+function verification (phone){
+	for (let i = 0; i < phone.length; i++){
+		if (isNaN(phone.charAt(i) == false)){
+			return 0;
+			break
+		}
+	}
+	return 1;
+} 
+
+while(true){
+let number = '+7' 
+number += prompt('Введите номер телефона без 8 и +7');
+	if (number.length === 12){
+		if (verification(number) == 1){
+			alert(formattedPhone(number)); // +7 (123) 456-78-90
+			break;
+		} else { alert('Вы ввели символы, повторите попытку снова используя только цифры!')}
+	} else { alert('Неверный формат номера телефона, должно быть 10 цифр, пожалуйста повторите попытку.')}
+}
